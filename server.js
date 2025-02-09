9ce774f602d9d7a8a2bf5460ca1a3d948766b126
Aditya <85333688+adityananu@users.noreply.github.com>
2025-02-09 13:54:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,5 +53,9 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+
 // Exporting the handler function for Vercel serverless
 module.exports = app;
